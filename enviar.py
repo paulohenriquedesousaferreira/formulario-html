7973809a5c3089b2305dd5bd7df0145cf3681401
Paulo Henrique <paullhsousa@gmail.com>
2025-05-05 19:44:30 -0300

    Deletado codigo python inutil

--- enviar.py
+++
@@ -1,4 +0,0 @@
-import pywhatkit
-
-# Envia mensagem para o número com DDI +55 (Brasil) e DDD (ex: 61)
-pywhatkit.sendwhatmsg("+5561981969785", "Olá! Essa mensagem foi enviada pelo Python!", 22, 30)
