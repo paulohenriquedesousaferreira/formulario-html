3d3cb57c3edd6a3dbb88558aa335a1fd63fb36a0
Paulo Henrique <paullhsousa@gmail.com>
2025-05-05 19:21:11 -0300

    Inicializado projeto

---
+++ enviar.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+# Envia mensagem para o número com DDI +55 (Brasil) e DDD (ex: 61)
+pywhatkit.sendwhatmsg("+5561981969785", "Olá! Essa mensagem foi enviada pelo Python!", 22, 30)
